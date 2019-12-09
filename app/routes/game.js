17110695327e00cfaf122230d1a2623365aa7fb5
Savino Rasola <savinorasola@Savinos-MBP.home>
2019-12-09 12:48:47 +0100

    display data from model

--- app/routes/game.js
+++ app/routes/game.js
@@ -2,12 +2,20 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return this.store.queryRecord('score', {})
-    .then(function(score) {
-      let username = score.get('player');
-      let playerScore = score.get('score');
-      console.log(`Currently logged in as ${username}`);
-      console.log(`Current score is: ${playerScore}`);
-    });    
+    return this.store.findAll('score');
+  },
+  actions: {
+    update() {
+      this.store.queryRecord('score', {})
+      .then(function(score) {
+        let username = score.get('player');
+        let playerScore = score.get('score');
+        console.log(`Currently logged in as ${username}`);
+        console.log(`Current score is: ${playerScore}`);
+      });
+      // debugger;
+      // rental.save();
+      // this.transitionTo('game');
+    }
   }
 });
